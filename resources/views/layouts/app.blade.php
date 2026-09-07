71f717f592582b68b8c9abd0532dec277d22416c
Victor Seixas Alves <vseixasalves@gmail.com>
2026-09-07 13:29:16 -0300

    Adição do favicon (Escudo do Clube)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }} — Painel</title>
+        <link rel="icon" type="image/png" href="{{ asset('images/EscudoFLU.png') }}">
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
